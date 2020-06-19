ca49d50f78c5d4d7fb100114fddd9924cb0630f2
Patrick Sodré <psodre@gmail.com>
2020-06-18 20:51:42 -0400

    Add AVX and CUDNN_HALF

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,16 @@ set -euxf
 GPU=0
 CUDNN=0
 OPENMP=1
+AVX=1
 OPTS=""
 
 if [[ "${cuda_compiler_version}" != "None" ]]; then
   GPU=1
   CUDNN=1
+  CUDNN_HALF=1
 fi
 
-make GPU=$GPU CUDNN=$CUDNN OPENMP=$OPENMP OPTS=$OPTS
+make GPU=$GPU CUDNN=$CUDNN CUDNN_HALF=$CUDNN_HALF $AVX=$AVX OPENMP=$OPENMP OPTS=$OPTS
 
 mkdir -p $PREFIX/bin
 install darknet -t $PREFIX/bin
