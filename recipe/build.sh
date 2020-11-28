83a191cd04d16af58a8a6271e31cff659977c77a
Patrick Sodré <psodre@gmail.com>
2020-11-28 15:41:17 -0500

    Fix build for CUDA 9.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,34 +1,36 @@
 set -euxf
 
-GPU=0
-CUDNN=0
-CUDNN_HALF=0
-ARCH=""
 OPENMP=1
 AVX=1
 LIBSO=1
 OPTS=""
 
+GPU=1
+CUDNN=1
+CUDNN_HALF=0
+ARCH="-gencode arch=compute_30,code=sm_30 \
+      -gencode arch=compute_35,code=sm_35 \
+      -gencode arch=compute_50,code=[sm_50,compute_50] \
+      -gencode arch=compute_53,code=[sm_53,compute_53] \
+      -gencode arch=compute_52,code=[sm_52,compute_52] \
+      -gencode arch=compute_60,code=[sm_60,compute_60] \
+      -gencode arch=compute_61,code=[sm_61,compute_61] \
+      -gencode arch=compute_62,code=[sm_62,compute_62] \
+      -gencode arch=compute_70,code=[sm_70,compute_70] \
+      -gencode arch=compute_72,code=[sm_72,compute_72]"
 
-if [[ "${cuda_compiler_version}" != "None" ]]; then
-  GPU=1
-  CUDNN=1
-  CUDNN_HALF=0
-  ARCH="-gencode arch=compute_30,code=sm_30 \
-        -gencode arch=compute_35,code=sm_35 \
-        -gencode arch=compute_50,code=[sm_50,compute_50] \
-        -gencode arch=compute_53,code=[sm_53,compute_53] \
-        -gencode arch=compute_52,code=[sm_52,compute_52] \
-        -gencode arch=compute_60,code=[sm_60,compute_60] \
-        -gencode arch=compute_61,code=[sm_61,compute_61] \
-        -gencode arch=compute_62,code=[sm_62,compute_62] \
-        -gencode arch=compute_70,code=[sm_70,compute_70] \
-        -gencode arch=compute_72,code=[sm_72,compute_72]"
-fi
-
-if [[ "${cuda_compiler_version}" > "10.0" ]]; then
-  ARCH="${ARCH} -gencode arch=compute_75,code=[sm_75,compute_75]"
-fi
+case ${cuda_compiler_version} in
+  None)
+    GPU=0
+    CUDNN=0
+    ARCH=""
+    ;;
+  9.2)
+    ;;
+  10.0|10.1|10.2)
+    ARCH="${ARCH} -gencode arch=compute_75,code=[sm_75,compute_75]"
+    ;;
+esac
 
 make LIBSO=1 GPU=$GPU CUDNN=$CUDNN CUDNN_HALF=$CUDNN_HALF ARCH="$ARCH" $AVX=$AVX OPENMP=$OPENMP OPTS=$OPTS vars all
 
